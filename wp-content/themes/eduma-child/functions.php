b9f467a222de291e0ca38551726e675fb39c85c6
kamauthomas <kamauthomas396@gmail.com>
2026-07-31 09:16:56 +0100

    fix: keep all Oldies faces in hero frame

--- wp-content/themes/eduma-child/functions.php
+++ wp-content/themes/eduma-child/functions.php
@@ -16,7 +16,7 @@ function toolkit_editorial_story_preview() {
  * URLs and triggers one server-side object/page-cache purge per environment.
  */
 function toolkit_theme_release() {
-	return '2026.07.31.4';
+	return '2026.07.31.5';
 }
 
 function toolkit_canonical_brand_name() {
