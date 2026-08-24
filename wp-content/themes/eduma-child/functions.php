8c94e1114502b2a1fe0b14fd4b982038f650277d
kamauthomas <kamauthomas396@gmail.com>
2026-08-24 11:33:17 +0300

    feat(catalog): publish approved 2026 fees

--- wp-content/themes/eduma-child/functions.php
+++ wp-content/themes/eduma-child/functions.php
@@ -24,7 +24,7 @@ function toolkit_editorial_story_preview() {
  * URLs and triggers one server-side object/page-cache purge per environment.
  */
 function toolkit_theme_release() {
-	return '2026.08.24.4';
+	return '2026.08.24.5';
 }
 
 function toolkit_is_demo_environment() {
